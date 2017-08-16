5622d78fbfe802b5bbdd4327fcf8e41e28ef4879
k186 <k1868548@gmail.com>
2017-08-16 16:47:23 +0800

    分页

--- src/nextPage/nextpage.js
+++ src/nextPage/nextpage.js
@@ -304,4 +304,24 @@
             })
         }
     };
+
+    template='<div class="nextpage">'+
+        '    <div class="nextpage-page">'+
+        '        <a class="nextpage-btn prev">上一页</a>'+
+        '        <ul class="nextpage-pagelist">'+
+        '        </ul>'+
+        '        <a class="nextpage-btn next">下一页</a>'+
+        '    </div>'+
+        '   <div class="nextpage-control">'+
+        '       <input type="text" class="nextpage-input"/>'+
+        '       <div class="nextpage-select">'+
+        '           <div class="nextpage-select-input">100</div>'+
+        '           <ul class="nextpage-select-chosen">'+
+        '               <li>30</li>'+
+        '               <li class="active">60</li>'+
+        '               <li>100</li>'+
+        '           </ul>'+
+        '       </div>'+
+        '   </div>'+
+        '</div>';
 });
